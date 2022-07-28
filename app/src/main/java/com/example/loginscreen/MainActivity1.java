3e2e5c31d6e50ce81d759f9d0de0b4c20a357842
unknown <you@example.com>
2022-07-28 17:43:16 +0530

    floating hint

--- app/src/main/java/com/example/loginscreen/MainActivity1.java
+++ app/src/main/java/com/example/loginscreen/MainActivity1.java
@@ -3,32 +3,45 @@ package com.example.loginscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity1 extends AppCompatActivity {
 
-TextInputEditText username;
+    TextInputEditText username;
+    TextInputLayout input_username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inputtextlayout);
-        username=findViewById(R.id.input_ed_txt);
-
+        username = findViewById(R.id.input_ed_txt);
+        input_username=findViewById(R.id.inputlayout);
+username.addTextChangedListener(new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
     }
-    public boolean validate(){
-       boolean valid=false;
-        String name=username.getText().toString();
-        if (username.length()<6){
-            valid= true;
-           username.setError(null);
 
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if(username.length()<6){
+
+input_username.setError("Minimum charcter length is 6");
         }else{
-            valid= false;
-            username.setError("Enter atleast 6 characters");
-
+            input_username.setError(null);
         }
-        return validate();
+
     }
+});
+    }
+
 }
